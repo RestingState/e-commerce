85e2eb6a8c02fda3ceedde1212e713e9e71195f9
Denys Zakharkevych <zaharkevich.denis@gmail.com>
2022-01-17 14:34:45 +0200

    differentiated connection logic

--- server/app.js
+++ server/app.js
@@ -3,16 +3,20 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const router = require('./routes/index');
+const connection = require('./db');
 const errorMiddleware = require('./middlewares/error-middleware');
 
 const app = express();
+connection();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-  credentials: true,
-  origin: process.env.CLIENT_URL
-}));
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL,
+  })
+);
 app.use('/api', router);
 app.use(errorMiddleware);
 
--- server/index.js
+++ server/index.js
@@ -1,14 +1,9 @@
 const app = require('./app');
 const mongoose = require('mongoose');
 
-const PORT = process.env.PORT || 5000;
-
 const start = async () => {
   try {
-    await mongoose.connect(process.env.DB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const PORT = process.env.PORT || 5000;
     app.listen(PORT, () => console.log(`Server started on PORT = ${PORT}`));
   } catch (e) {
     console.log(e);

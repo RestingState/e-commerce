37e695eb80f15f3def481197ac93721c47d032be
Denys Zakharkevych <zaharkevich.denis@gmail.com>
2022-01-17 17:09:04 +0200

    prettier formating

--- server/routes/customer-router.js
+++ server/routes/customer-router.js
@@ -1,49 +1,49 @@
-const Router = require("express");
+const Router = require('express');
 const router = new Router();
-const customerController = require("../controllers/customer-controller");
-const { body } = require("express-validator");
-const authMiddleware = require("../middlewares/auth-middleware");
+const customerController = require('../controllers/customer-controller');
+const { body } = require('express-validator');
+const authMiddleware = require('../middlewares/auth-middleware');
 
 router.post(
-  "/registration",
-  body("login").isLength({ min: 5, max: 32 }),
-  body("password").isLength({ min: 5, max: 32 }),
-  body("email").isEmail(),
+  '/registration',
+  body('login').isLength({ min: 5, max: 32 }),
+  body('password').isLength({ min: 5, max: 32 }),
+  body('email').isEmail(),
   customerController.registration
 );
-router.post("/login", customerController.login);
-router.post("/logout", customerController.logout);
-router.get("/activate/:link", customerController.activate);
-router.get("/refresh", customerController.refresh);
-router.delete("", authMiddleware, customerController.deleteCustomer);
-router.get("/personalInfo", authMiddleware, customerController.getPersonalInfo);
+router.post('/login', customerController.login);
+router.post('/logout', customerController.logout);
+router.get('/activate/:link', customerController.activate);
+router.get('/refresh', customerController.refresh);
+router.delete('', authMiddleware, customerController.deleteCustomer);
+router.get('/personalInfo', authMiddleware, customerController.getPersonalInfo);
 router.patch(
-  "/personalInfo",
+  '/personalInfo',
   authMiddleware,
   customerController.updatePersonalInfo
 );
 router.post(
-  "/createOrderReceiver",
+  '/createOrderReceiver',
   authMiddleware,
   customerController.createOrderReceiver
 );
 router.patch(
-  "/updateOrderReceiver/:id",
+  '/updateOrderReceiver/:id',
   authMiddleware,
   customerController.updateOrderReceiver
 );
 router.delete(
-  "/deleteOrderReceiver/:id",
+  '/deleteOrderReceiver/:id',
   authMiddleware,
   customerController.deleteOrderReceiver
 );
 router.get(
-  "/getOrderReceivers/:id",
+  '/getOrderReceivers/:id',
   authMiddleware,
   customerController.getOrderReceivers
 );
 router.get(
-  "/orderReceiver/primary",
+  '/orderReceiver/primary',
   authMiddleware,
   customerController.getPrimaryOrderReceiver
 );
